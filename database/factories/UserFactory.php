ba4090b2054d4cbfa09c6a4796b7c06300ef7cbe
Burak Hamza <thedatcal@gmail.com>
2022-12-19 23:44:39 +0100

    fix: create user factory and seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,10 +19,13 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'username' => $this->faker->userName,
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'photo' => $this->faker->image('public/storage/images',640,480, null, false),
+            'description' => $this->faker->text($maxNbChars = 200),
         ];
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,14 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $usersCount = (int)$this->command->ask('How many users do you need ? Default : ', 10);
+        $linksCount = (int)$this->command->ask('How many links do you need ? Default : ', 5);
+        \App\Models\User::factory()
+            ->count($usersCount)
+            ->has(\App\Models\Link::factory()->count($linksCount))
+            ->create();
+        $this->command->info('Users Created!');
+        $this->command->info('Users Links Added!');
     }
 }
