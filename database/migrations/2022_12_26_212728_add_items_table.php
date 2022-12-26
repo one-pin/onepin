23ce9752ce14a2b8725742e55d22f8e2904b2332
Burak Hamza <thedatcal@gmail.com>
2022-12-26 22:57:03 +0100

    fix: add items table

---
+++ database/migrations/2022_12_26_212728_add_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->enum('media_type',array('link','picture','video','widget'));
+            $table->integer('media_id')->default(0);
+            $table->enum('location',array('main','other'));
+            $table->integer('location_id')->nullable();
+            $table->string('title')->nullable();
+            $table->string('url')->nullable();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+};
